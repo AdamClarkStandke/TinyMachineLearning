cc50f0d4ed9d27fda6d4167e9c62897a63aa25d5
Adam Standke <38574429+AdamClarkStandke@users.noreply.github.com>
2023-04-01 19:17:28 -0400

    Delete person_detection_OV2640Mini2MPlus directory

--- person_detection_OV2640Mini2MPlus/arduino_main.cpp
+++
@@ -1,9 +0,0 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Resed.
-
-==============================================================================*/
-
-#include "main_functions.h"
-
-// Arduino automatically calls the setup() and loop() functions in a sketch, so
-// where other systems need their own main routine in this file, it can be left
-// empty.
