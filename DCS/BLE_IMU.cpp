e4ee4bc4b8cf3e946d840f6fbd18a537433fdd1a
Adam Standke <38574429+AdamClarkStandke@users.noreply.github.com>
2023-10-15 23:52:50 -0400

    Add files via upload
    Working with the MaahonyAHRS filter to not get wrong values for roll, pitch, and yaw

--- DCS/BLE_IMU.cpp
+++ DCS/BLE_IMU.cpp
@@ -1,3 +1,10 @@
+/* 
+Most of this code comes from dhir_33 when posted
+in the arduino forum https://forum.arduino.cc/t/spikes-when-using-mahony-filter/1106933
+In this case, I am not using adafruit's ahrs library but rather
+the Mahony AHRS filter as maintained by PaulStoffregen and njh as found here:
+https://github.com/PaulStoffregen/MahonyAHRS
+*/
 #include <MahonyAHRS.h>
 #include "BLE_IMU.h"
 #include <Wire.h>
@@ -8,6 +15,7 @@ float Acc[3], Gyro[3], Mag[3];         // raw IMU data
 float   throttleCorrection;
 Mahony filter;
 MyBoschSensor myIMU(Wire);
+//float gyroDriftX, gyroDriftY, gyroDriftZ;  // units dps
 
 void read_data() {
   if (myIMU.accelerationAvailable()){
@@ -21,18 +29,41 @@ void read_data() {
   }
 }
 
+// void calibrateIMU(int delayMillis, int calibrationMillis) {
+//   int calibrationCount = 0;
+//   delay(delayMillis);  // to avoid shakes after pressing reset button
+//   float sumX, sumY, sumZ;
+//   int startTime = millis();
+//   while (millis() < startTime + calibrationMillis) {
+//     read_data();
+//     // in an ideal world gyroX/Y/Z == 0, anything higher or lower represents drift
+//     sumX += gyroX;
+//     sumY += gyroY;
+//     sumZ += gyroZ;
+//     calibrationCount++;
+//   }
+
+//   if (calibrationCount == 0) {
+//     Serial.println("Failed to calibrate");
+//   }
+
+//   gyroDriftX = sumX / calibrationCount;
+//   gyroDriftY = sumY / calibrationCount;
+//   gyroDriftZ = sumZ / calibrationCount;
+// }
+
 void initIMU(){
   //callback function to read data from
   myIMU.onInterrupt(read_data);
   myIMU.begin();
-  filter.begin(100); // filter to expect 100 measurements per second
+  //calibrateIMU(250, 250);
+  filter.begin(1000); // filter to expect 100 measurements per second
 }
 
 void calcIMU(){
-
-  // TODO: calibrate the Bosh gyroscope for scaling 
-  // Update the Mahony filter, with scaled gyroscope
-  float gyroScale = 0.001;  // TODO: the filter updates too fast
+  // BMI270 gyroscope uses degrees/second
+  // Mahony expects gyroscope in radians/second
+  float gyroScale = 3.14159f / 180.0f;
   Gyro[0] =  Gyro[0] * gyroScale;
   Gyro[1] =  Gyro[1] * gyroScale;
   Gyro[2] =  Gyro[2] * gyroScale;
@@ -44,9 +75,11 @@ void calcIMU(){
   // Debug IMU values
   Serial.print("IMURoll:");
   Serial.println(IMURoll);
+  delay(20);
   Serial.print("IMUPitch:");
   Serial.println(IMUPitch);
-  Serial.print("IMUHead:");
-  Serial.println(IMUHead);
+  delay(20);
+  //Serial.print("IMUHead:");
+  //Serial.println(IMUHead);
 }
 
